69a02df5a93c8ec20bd8d061e51e900b981254b4
Joshua Plischek <plischekjoshua@web.de>
2025-10-22 19:21:35 +0200

    add fonts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ImprintComponent } from './main/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './main/privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainComponent},
+    {path: 'imprint', component: ImprintComponent},
+    {path: 'privacy-policy', component: PrivacyPolicyComponent},
+
+];
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HeroComponent } from "./hero/hero.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, HeroComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
