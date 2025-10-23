b5a2249b34f8f8aa1aa0cddfbc78fd9e9681f6eb
Joshua Plischek <plischekjoshua@web.de>
2025-10-23 12:07:57 +0200

    finished sections from hero to about

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
+import { LeistungenComponent } from "./leistungen/leistungen.component";
+import { AboutComponent } from "./about/about.component";
+import { TestimonialsComponent } from "./testimonials/testimonials.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, LeistungenComponent, AboutComponent, TestimonialsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
